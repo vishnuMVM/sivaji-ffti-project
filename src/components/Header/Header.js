0da05da01c35fdd03a4f7fc4aa9753e8d2443749
vishnu <yvishnuvardhan325@gmail.com>
2021-12-16 18:34:50 +0530

    upto navbar

---
+++ src/components/Header/Header.js
@@ -0,0 +1,95 @@
+import {Link} from "react-router-dom";
+import "./Header.css";
+import { Component } from "react";
+
+
+
+class Header extends Component{
+    state={sidebar:false}
+
+    Sidebartoggle=()=>{
+        this.setState((prevState)=>{  
+            let sidebar=!prevState.sidebar;
+            return{sidebar,};
+})
+    }
+
+
+    
+    render(){
+        const {sidebar}=this.state;
+        return(
+            <div>
+             <div className="header-container header-desktop">
+                 <div className="header-image-container">
+                     <div className="logo-container">
+                        company logo comes here
+                     </div>
+
+                     <div>
+                        <h1> FFTI</h1>
+                     </div>
+
+                 </div>
+         
+                 <div className="header-list-selection-container">
+             
+                     <div>
+                         <ul className="header-list-items-container">
+                             <li><Link className="header-list-item" to="/">Home</Link></li>
+                             <li><Link className="header-list-item" to="/achivements">Achivements</Link></li>
+                             <li><Link className="header-list-item" to="/grow-with-us">Grow with us</Link></li>
+                             <li><Link className="header-list-item" to="/management">Management</Link></li>
+                             <li><Link className="header-list-item" to="/storelocation">Store Location</Link></li>
+                         </ul>
+                     </div>
+                 </div>
+             </div>
+         
+         
+             
+             <div className="header-mobile">
+                 <div className={sidebar?"sidebar_display":"sidebar_hide"}>
+                     <div className="sidebar_button_container">
+                     <button onClick={this.Sidebartoggle} className="sidebar_button">X</button>
+                     </div>
+                     <div className="sidebar-container">
+                            <p> <Link className="header-list-item" to="/">Home </Link></p>
+                            <p> <Link className="header-list-item" to="/achivements">Achivements </Link></p>
+                            <p><Link className="header-list-item" to="/grow-with-us">Grow with us </Link></p>
+                            <p> <Link className="header-list-item" to="/management">Management </Link></p>
+                            <p> <Link className="header-list-item" to="/storelocation">Store Location </Link></p>
+                     </div>
+                 </div>
+         
+             <div className="header-container">
+                 <div className="header-image-container">
+                    <div className="logo-container">
+                        company logo 
+                     </div>
+
+                     <div>
+                        <h1> FFTI</h1>
+                     </div>
+                 </div>
+         
+                 
+             
+                     <div>
+                         <button onClick={this.Sidebartoggle} className="header-button" > <i class="fa fa-bars"></i></button>
+                     </div>
+                 
+             </div>
+         
+         
+             </div>
+     </div>
+         
+         )
+         
+
+    }
+
+}
+
+export default Header;
