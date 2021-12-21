9691b00b702bde6ee64cb49f6f8aa73e0d87999d
vishnu <yvishnuvardhan325@gmail.com>
2021-12-21 12:08:08 +0530

    grow with us initiated

--- src/components/GrowWithUs/GrowWithUs.js
+++ src/components/GrowWithUs/GrowWithUs.js
@@ -5,6 +5,32 @@ export default function GrowWithUs (){
 
   return(
     <div className="gwu-main-container">
+      <div>
+      <p className="gwu-text">
+        FFTI will always be there to encourage talented aspirants<br/>
+        FFTI have heard several stories of franchisors of its own 
+        who have been successful in their business, you can also be one of them or top of them if you join with us.
+      </p>
+      </div>
+
+      <div className="franchise-main-container">
+        <div className="franchise-ind-container">
+          <img className='store-img' src="https://image.shutterstock.com/image-photo/haberdasher-fabric-shop-choice-textile-260nw-1757940401.jpg" alt="store-img"/>
+          <p className='store-text'>trupati</p>
+        </div>
+
+        <div className="franchise-ind-container">
+          <img className='store-img' src="https://image.shutterstock.com/image-photo/haberdasher-fabric-shop-choice-textile-260nw-1757940401.jpg" alt="store-img"/>
+          <p className='store-text'>trupati</p>
+        </div>
+
+        <div className="franchise-ind-container">
+          <img className='store-img' src="https://image.shutterstock.com/image-photo/haberdasher-fabric-shop-choice-textile-260nw-1757940401.jpg" alt="store-img"/>
+          <p className='store-text'>trupati</p>
+        </div>
+        
+      </div>
+
   
     </div>
 
