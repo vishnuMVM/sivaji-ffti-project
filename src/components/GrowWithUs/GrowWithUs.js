b852c17516b824d724ec2374fbd4b5ee85e7f1f6
vishnu <yvishnuvardhan325@gmail.com>
2021-12-22 15:31:55 +0530

    marquee is added

--- src/components/GrowWithUs/GrowWithUs.js
+++ src/components/GrowWithUs/GrowWithUs.js
@@ -5,12 +5,14 @@ export default function GrowWithUs (){
 
   return(
     <div className="gwu-main-container">
-      <div>
+      <div className="top-text-container">
       <p className="gwu-text">
-        FFTI will always be there to encourage talented aspirants
-        FFTI have heard several stories of franchisors of its own 
-        who have been successful in their business, you can also be one of them or top of them if you join with us.
+        FFTI will always be there to encourage talented aspirants.
+        FFTI have heard several stories of franchisors of its own ,
+        who have been successful in their business. You can also be one of them or top of them, if you join with us.
       </p>
+        <marquee behavior="scroll" direction="left" onmouseover="this.stop();" onmouseout="this.start();">YOU CAN GRAB YOUR OPPURTUNITY BY CONTACTING US THROW MANAGEMENT PAGE</marquee>
+      
       </div>
 
       <div className="franchise-main-container">
@@ -28,6 +30,22 @@ export default function GrowWithUs (){
           <img className='store-img' src="https://image.shutterstock.com/image-photo/haberdasher-fabric-shop-choice-textile-260nw-1757940401.jpg" alt="store-img"/>
           <p className='store-text'>trupati</p>
         </div>
+        <div className="franchise-ind-container">
+          <img className='store-img' src="https://image.shutterstock.com/image-photo/haberdasher-fabric-shop-choice-textile-260nw-1757940401.jpg" alt="store-img"/>
+          <p className='store-text'>trupati</p>
+        </div>
+
+        <div className="franchise-ind-container">
+          <img className='store-img' src="https://image.shutterstock.com/image-photo/haberdasher-fabric-shop-choice-textile-260nw-1757940401.jpg" alt="store-img"/>
+          <p className='store-text'>trupati</p>
+        </div>
+
+        <div className="franchise-ind-container">
+          <img className='store-img' src="https://image.shutterstock.com/image-photo/haberdasher-fabric-shop-choice-textile-260nw-1757940401.jpg" alt="store-img"/>
+          <p className='store-text'>trupati</p>
+        </div>
+
+
         
       </div>
 
