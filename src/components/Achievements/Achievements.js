4d1aba07876ef1b620d62b126d935d190e2bd66c
vishnu <yvishnuvardhan325@gmail.com>
2021-12-23 17:02:11 +0530

    achievement page intiated

--- src/components/Achievements/Achievements.js
+++ src/components/Achievements/Achievements.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import Content from '../Content';
-
+import "./Achievements.css"
 export default function Achievements (){
 
   return(
+    <div>
+      <h1 style={{textAlign:'center', color:"#9b1313"}}>Futures First Texties India</h1>
+      <div className="ach-intro-text">There are two kinds of people, leaders and followers.
+        The followers go along with the bandwagon, unlike the leaders who carve their own path, 
+        map their own road and embark on a journey of success. Reddipalli Sivaji and Chandagani Mahendra are one among them.
+        <br/>  <br/>
+        These two youngsters are self-made enterprenuers, 
+        who always dreamed to achieve big from their childhood.
+        They started earning when they were students and they both met at hyderabad while they were working for 
+        a mlm company, from then they travelled all the way to what they are now.
+        <br/>  <br/>
+        Having realised a fact that textile industry is the one never goes out of fashion, 
+        they started FFTI on FEB 13th 2012 and stood number 1 in the market from decade.
+        Their passion towards women empowerment gave opportunity for nearby womens to work and become independent.  
+      </div>
 
-    <Content />
+    </div>
 
   )
 
--- src/components/GrowWithUs/GrowWithUs.js
+++ src/components/GrowWithUs/GrowWithUs.js
@@ -11,38 +11,40 @@ export default function GrowWithUs (){
         FFTI have heard several stories of franchisors of its own ,
         who have been successful in their business. You can also be one of them or top of them, if you join with us.
       </p>
-        <marquee behavior="scroll" direction="left" onmouseover="this.stop();" onmouseout="this.start();">YOU CAN GRAB YOUR OPPURTUNITY BY CONTACTING US THROW MANAGEMENT PAGE</marquee>
+        <marquee behavior="scroll" direction="left">
+          YOU CAN GRAB YOUR OPPORTUNITY BY CONTACTING US THROUGH MANAGEMENT PAGE
+        </marquee>
       
       </div>
 
       <div className="franchise-main-container">
         <div className="franchise-ind-container">
-          <img className='store-img' src="https://image.shutterstock.com/image-photo/haberdasher-fabric-shop-choice-textile-260nw-1757940401.jpg" alt="store-img"/>
-          <p className='store-text'>trupati</p>
+          <img className='store-img' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSavnG53v9VShRYJOjX45mgRcj1m8JovZiVdA&usqp=CAU" alt="store-img"/>
+          <p className='store-text'>Tadipatri</p>
         </div>
 
         <div className="franchise-ind-container">
-          <img className='store-img' src="https://image.shutterstock.com/image-photo/haberdasher-fabric-shop-choice-textile-260nw-1757940401.jpg" alt="store-img"/>
-          <p className='store-text'>trupati</p>
+          <img className='store-img' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQF-Ry5awogpcTmqBYyOphTyzGX4CpeiOKhGg&usqp=CAU" alt="store-img"/>
+          <p className='store-text'>Dharmavaram</p>
         </div>
 
         <div className="franchise-ind-container">
-          <img className='store-img' src="https://image.shutterstock.com/image-photo/haberdasher-fabric-shop-choice-textile-260nw-1757940401.jpg" alt="store-img"/>
-          <p className='store-text'>trupati</p>
+          <img className='store-img' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSW-3xYIFXNK7mQE-HQtDZ_GtxL0ipTonl4QQ&usqp=CAU" alt="store-img"/>
+          <p className='store-text'>Jagityal</p>
         </div>
         <div className="franchise-ind-container">
-          <img className='store-img' src="https://image.shutterstock.com/image-photo/haberdasher-fabric-shop-choice-textile-260nw-1757940401.jpg" alt="store-img"/>
-          <p className='store-text'>trupati</p>
-        </div>
-
-        <div className="franchise-ind-container">
-          <img className='store-img' src="https://image.shutterstock.com/image-photo/haberdasher-fabric-shop-choice-textile-260nw-1757940401.jpg" alt="store-img"/>
-          <p className='store-text'>trupati</p>
+          <img className='store-img' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAaLpVH2Nvqot_JWwdM4lwSpQ_F9YxLkEr3w&usqp=CAU" alt="store-img"/>
+          <p className='store-text'>Proddatur</p>
         </div>
 
         <div className="franchise-ind-container">
-          <img className='store-img' src="https://image.shutterstock.com/image-photo/haberdasher-fabric-shop-choice-textile-260nw-1757940401.jpg" alt="store-img"/>
-          <p className='store-text'>trupati</p>
+          <img className='store-img' src="https://lh3.googleusercontent.com/YwTp5NNRNfXAQhE9GO8UEP6iW3PnxF_QBVsWkk6bWx9XX4lDrn4KmqqeCXOpIZF6u4lrJluw=w1080-h608-p-no-v0" alt="store-img"/>
+          <p className='store-text'>Papely,kurnool</p>
+        </div>
+
+        <div className="franchise-ind-container">
+          <img className='store-img' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqWZr6CibcrezT5r3V7aaZNzxn4H05mrfJMC0v14ZZ4N9Rh0V1j8G-IsCtmCU9t-G3WT0&usqp=CAU" alt="store-img"/>
+          <p className='store-text'>Dilsuknagar, Hyderabad</p>
         </div>
 
 
