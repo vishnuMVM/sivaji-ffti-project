217ec5728435ed36b854ce86b3a838a18f26a422
LokeshDama <damalokeshskht@gmail.com>
2022-01-24 12:41:50 +0530

    achievements page updated

--- src/components/Achievements/Achievements.js
+++ src/components/Achievements/Achievements.js
@@ -1,29 +1,39 @@
 import React from 'react';
 import "./Achievements.css"
+import article from './article.jpg'
 export default function Achievements (){
 
   return(
     <div>
       <h1 style={{textAlign:'center', color:"#9b1313"}}>Futures First Texties India</h1>
-      <div className="ach-intro-text">There are two kinds of people, leaders and followers.
+      <div className="ach-intro-text">
+      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-brightness-high-fill text-secondary" viewBox="0 0 16 16">
+  <path d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
+</svg> There are two kinds of people, leaders and followers.
         The followers go along with the bandwagon, unlike the leaders who carve their own path, 
         map their own road and embark on a journey of success. Reddipalli Sivaji and Chandagani Mahendra are one among them.
         <br/>  <br/>
-        These two youngsters are self-made enterprenuers, 
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-brightness-high-fill text-secondary" viewBox="0 0 16 16">
+  <path d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
+</svg> These two youngsters are self-made enterprenuers, 
         who always dreamed to achieve big from their childhood.
         They started earning when they were students and they both met at hyderabad from then they travelled all the way to what they are now.
         <br/>  <br/>
-        Having realised a fact that textile industry is the one never goes out of fashion, 
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-brightness-high-fill text-secondary" viewBox="0 0 16 16">
+  <path d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
+</svg>  Having realised a fact that textile industry is the one never goes out of fashion, 
         they started FFTI on FEB 13th 2012 and stood number 1 in the market from decade.
         Their passion towards women empowerment gave opportunity for nearby womens to work and become independent.
         <br/> <br/>
-        They have a enterprenuer mindset rather than a employee which provided employment for 80 members directly
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-brightness-high-fill text-secondary" viewBox="0 0 16 16">
+  <path d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
+</svg> They have a enterprenuer mindset rather than a employee which provided employment for 80 members directly
         and 120 members indirectly.  
       </div>
       <div className='ach-table'>
         <div className='ach-tr'>
           <div className='ach-td'>
-            <h2 className='table-txt'>*Media recognised FFTI services, at the time of pandamec
+            <h2 className='table-txt'>Media recognised FFTI services, at the time of pandamec
             </h2>
           </div>
           <div >
@@ -33,7 +43,7 @@ export default function Achievements (){
 
         <div className='ach-tr'>
           <div className='ach-td'>
-            <h2 className='table-txt'>*ycp cricket cup event,hudihudihausiuuihui
+            <h2 className='table-txt'>Enclothed Sports Outfit for YCP Cricket Cup Event.
             </h2>
           </div>
           <div >
@@ -41,6 +51,18 @@ export default function Achievements (){
           </div>
         </div>
        
+       </div>
+
+       <div className='container'>
+              <div className='row d-flex flex-row'>
+                  <div className='col-6 mb-5'>
+              <img className='w-100' src={article} alt='..'/>
+                  </div>
+                  <div className='col-6 mb-5'>
+                    <p className='article'>Featured in a Research Article by <strong> journalijcir.com </strong>. They have Made a case study on our <em><strong>FFTI Sports</strong></em>. Check out the article
+                    <a href='https://firebasestorage.googleapis.com/v0/b/lokesh-vishnu-consignment.appspot.com/o/Article%2F00581-A-2017.pdf?alt=media&token=9e9f8f9f-1220-4844-8cbd-b4afc7a5c378' target="_blank" > here </a> for more info.</p>
+                  </div>
+              </div>
        </div>
 
     </div>
